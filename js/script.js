61b694e20d8dfe29d0634818cf20cf5a14886b71
onth0s <metaagnosis@gmail.com>
2026-06-04 13:57:09 +0200

    quicksave

--- js/script.js
+++ js/script.js
@@ -3,84 +3,9 @@ let scrollBarSync = false;
 let mermaidRenderTimer = null;
 let mermaidRenderVersion = 0;
 
-const defaultInput = `# Markdown syntax guide
+const defaultInputFallback = `# Markdown Viewer
 
-## Headers
-
-# This is a Heading h1
-## This is a Heading h2
-###### This is a Heading h6
-
-## Emphasis
-
-*This text will be italic*  
-_This will also be italic_
-
-**This text will be bold**  
-__This will also be bold__
-
-_You **can** combine them_
-
-## Lists
-
-### Unordered
-
-* Item 1
-* Item 2
-* Item 2a
-* Item 2b
-    * Item 3a
-    * Item 3b
-
-### Ordered
-
-1. Item 1
-2. Item 2
-3. Item 3
-    1. Item 3a
-    2. Item 3b
-
-## Images
-
-![This is an alt text.](/image/Markdown-mark.svg "This is a sample image.")
-
-## Links
-
-You may be using [Markdown Viewer](https://markdownlivepreview.com/).
-
-## Blockquotes
-
-> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
->
->> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
-
-## Tables
-
-| Left columns  | Right columns |
-| ------------- |:-------------:|
-| left foo      | right foo     |
-| left bar      | right bar     |
-| left baz      | right baz     |
-
-## Blocks of code
-
-\`\`\`
-let message = 'Hello world';
-alert(message);
-\`\`\`
-
-## Mermaid diagrams
-\`\`\`mermaid
-graph TD
-  A[Start] --> B{Decision}
-  B -->|Yes| C[Finish]
-  B -->|No| D[Alternate]
-\`\`\`
-
-## Inline code
-
-This web site is using \`markedjs/marked\`.
-`;
+Start writing markdown...`;
 
 const STORAGE_THEME_KEY = 'com.markdownlivepreview_theme';
 const STORAGE_CONTENT_KEY = 'mdv_last_content';
@@ -286,12 +211,10 @@ let presetValue = (value) => {
   editor.scrollTop = 0;
   syncHighlight();
   editor.focus();
-  hasEdited = value !== defaultInput;
   convert(value);
 };
 
 editor.addEventListener('input', () => {
-  if (editor.value !== defaultInput) hasEdited = true;
   syncHighlight();
   convert(editor.value);
   saveLastContent(editor.value);
@@ -600,19 +523,26 @@ let setupDivider = () => {
 
 let lastContent = loadLastContent();
 initMermaid();
-if (lastContent) {
-  presetValue(lastContent);
-} else {
-  presetValue(defaultInput);
-}
-
 let savedScrolls = loadScrollPositions();
-if (savedScrolls) {
-  applyScrollRatio(editor, savedScrolls.editor);
-  editorHighlight.scrollTop = editor.scrollTop;
-  renderMermaidDiagrams().then(() => {
-    applyScrollRatio(document.getElementById('preview'), savedScrolls.preview);
-  });
+
+let onContentReady = (content) => {
+  presetValue(content);
+  if (savedScrolls) {
+    applyScrollRatio(editor, savedScrolls.editor);
+    editorHighlight.scrollTop = editor.scrollTop;
+    renderMermaidDiagrams().then(() => {
+      applyScrollRatio(document.getElementById('preview'), savedScrolls.preview);
+    });
+  }
+};
+
+if (lastContent) {
+  onContentReady(lastContent);
+} else {
+  fetch('DEFAULT.md')
+    .then(r => r.ok ? r.text() : Promise.reject(new Error('HTTP ' + r.status)))
+    .then(text => onContentReady(text))
+    .catch(() => onContentReady(defaultInputFallback));
 }
 
 editor.addEventListener('scroll', saveScrollPositions, { passive: true });
